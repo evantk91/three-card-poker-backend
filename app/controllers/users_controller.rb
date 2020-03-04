9de036caf34b711279a303c88f0b6009dbafb166
etgreer91 <etgreer91@gmail.com>
2020-03-04 09:48:55 -0700

    updates create action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create user_params
-        render json: {user: @user}
+        render json: @user
     end
 
     private
